c9c4dc07d74b0322ffd96605d9a546ceb226b4bd
RahimFatih <65657905+RahimFatih@users.noreply.github.com>
2021-04-23 16:15:52 +0200

    INIT

---
+++ Models/Game.cs
@@ -0,0 +1,9 @@
+public class Game
+    {
+        public int ID { get; set; }
+        public string Tytuł { get; set; }
+        public DateTime DataWydania { get; set; }
+        public string Gatunek { get; set; }
+        public string Platforma { get; set; }
+        public decimal Cena { get; set; }
+    }
